c4629125a43a0dc20af0848273647bd1e9e32a90
katiecox4 <89658665+katiecox4@users.noreply.github.com>
2022-06-06 14:53:33 -0700

    hello world

---
+++ src/helloworld.java
@@ -0,0 +1,11 @@
+
+
+class hello {
+
+    public static void main(String[] args) {
+        //user interface could just go here
+        System.out.println("hello world");
+    }
+}
+//hi, this is a comment
+/*this is also a comment*/
